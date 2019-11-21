097608c39328ba2188e35e637f67684cf75fe62f
victoria <victoria.edon@gmail.com>
2019-11-21 20:00:10 +0700

    add ktlint and improve code formatting

--- app/src/androidTest/java/com/victoriaedon/oldtwitterlayout/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/victoriaedon/oldtwitterlayout/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package com.victoriaedon.oldtwitterlayout
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
--- app/src/main/java/com/victoriaedon/oldtwitterlayout/MainActivity.kt
+++ app/src/main/java/com/victoriaedon/oldtwitterlayout/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.victoriaedon.oldtwitterlayout
-
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
